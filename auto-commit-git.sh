e128f9f593dc71910714005f1f42aab0317282cb
engrron30 <ronaldtorresrosete30@gmail.com>
2025-09-09 10:18:22 +0800

    Add dummy_file_1.dat

--- auto-commit-git.sh
+++ auto-commit-git.sh
@@ -6,10 +6,14 @@ for ((i = 1; i <= $NUM_OF_ITERATIONS; i++)); do
 
     # Create file then commit
     echo "Dummy-Text-for-Commit" >>dummy_file_$i.dat
+    git add ./
     git commit -m "Add dummy_file_$i.dat"
+    git push -u origin main
 
     # Remove file then commit
     rm dummy_file_$i.dat
+    git add ./
     git commit -m "Remove dummy_file_$i.dat"
+    git push -u origin main
 
 done
