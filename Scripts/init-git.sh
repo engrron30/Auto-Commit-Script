8972268e9051ba4a4a77ef4c9904cf2edb4f12e5
engrron30 <ronaldtorresrosete30@gmail.com>
2025-09-09 09:43:45 +0800

    Use GIT_CMD for script commands

--- Scripts/init-git.sh
+++ Scripts/init-git.sh
@@ -2,10 +2,12 @@
 # This newly created repo must be empty. This script
 # file will initialize your empty repo with a README.md.
 
+GIT_CMD=git
+
 echo "# Auto-Commit-Script" >>README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:engrron30/Auto-Commit-Script.git
-git push -u origin main
+$GIT_CMD init
+$GIT_CMD add README.md
+$GIT_CMD commit -m "first commit"
+$GIT_CMD branch -M main
+$GIT_CMD remote add origin git@github.com:engrron30/Auto-Commit-Script.git
+$GIT_CMD push -u origin main
