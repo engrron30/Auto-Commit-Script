8e3d4b19d8f21f8055f0f10db387f033063f14e2
engrron30 <ronaldtorresrosete30@gmail.com>
2025-09-09 10:16:41 +0800

    Update auto-commit script

--- auto-commit-git.sh
+++ auto-commit-git.sh
@@ -3,5 +3,13 @@
 NUM_OF_ITERATIONS=15
 
 for ((i = 1; i <= $NUM_OF_ITERATIONS; i++)); do
+
+    # Create file then commit
     echo "Dummy-Text-for-Commit" >>dummy_file_$i.dat
+    git commit -m "Add dummy_file_$i.dat"
+
+    # Remove file then commit
+    rm dummy_file_$i.dat
+    git commit -m "Remove dummy_file_$i.dat"
+
 done
